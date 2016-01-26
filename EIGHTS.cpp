5cd8172bdce811fa6228d4e8ab05839b557df203
Heena Jain <heena.jain98@gmail.com>
2016-01-26 18:07:49 +0530

    EIGHTS.cpp

---
+++ EIGHTS.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	long long int t,ans;
+	int n;
+	cin>>n;
+	while(n--)
+	{
+	cin>>t;
+	ans=192+(t-1)*250;
+	cout<<ans<<endl;
+}
+return 0;
+
+}
